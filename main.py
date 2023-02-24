67ba5dc4f3a14281e23ade5ee36af8372b8296b8
Valdeí Alves Lins <94989958+valdei97@users.noreply.github.com>
2023-02-24 00:10:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from random import randint
+
+print ('Jogo de Adivinhação')
+pc = randint (1,10)
+user = 0 #variavel iniciada fora do laço
+tentativas = 1
+vidas_user = 3 # vidas do jogador
+
+while not user == pc:
+  if vidas_user == 0:
+    print (f'Game Over!! {vidas_user} vida(s)')
+    break
+  user = int (input('Digite um número: '))
+  print ('=-' * 10)
+  if user < pc:
+    print (f'Errou, o número é MAIOR que {user}.')
+    tentativas += 1
+    vidas_user -= 1
+    print(f'Você tem mais {vidas_user} vida(s)')
+  elif user > pc:
+    print (f'Errou, o número é MENOR que {user}.')
+    tentativas += 1
+    vidas_user -= 1
+    print(f'Você tem mais {vidas_user} vida(s)')
+  elif user == pc: #usei elif para ficar mais fácil de entender#
+    print ('Acertou.. Parabéns')
+    print (f'Acertou com {tentativas} tentativas')
+    break
+  
+ 
